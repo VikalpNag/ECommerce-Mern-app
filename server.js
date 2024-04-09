d17e336e2731d0d59a7a0dba9de6afa69aa615b6
Vikalp Nag <vikalpnag@gmail.com>
2024-04-09 18:40:39 +0530

    user Schema & register route

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import color from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
 
 //DB config
 connectDB();
@@ -12,7 +13,10 @@ const app = express();
 
 //middlewares
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
+
+//routes
+app.use('/api/v1/auth',authRoutes);
 
 //configure env
 dotenv.config();
