e16342d0c9c594da2338e19c79f005639568b00f
Vikalp Nag <vikalpnag@gmail.com>
2024-04-14 20:45:13 +0530

    components/pages/router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import Layout from "./Components/Layout/Layout";
+import { Routes, Route } from "react-router-dom";
+import About from "./Pages/About.jsx";
+import Contact from "./Pages/Contact.jsx";
+import HomePage from "./Pages/HomePage.jsx";
+import PageNotFound from "./Pages/PageNotFound";
+import Policy from "./Pages/Policy.jsx";
+
 function App() {
   return (
     <>
-      <h1>Ecommerce App</h1>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
