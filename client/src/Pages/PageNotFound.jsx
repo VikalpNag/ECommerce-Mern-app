dcecddae7bd9913ec0a7de3664771a98a83bb237
Vikalp Nag <vikalpnag@gmail.com>
2024-04-23 20:39:23 +0530

    Pnf component

--- client/src/Pages/PageNotFound.jsx
+++ client/src/Pages/PageNotFound.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Layout from "../Components/Layout/Layout";
+import { Link } from "react-router-dom";
+import "./css/PageNotFound.css"
 
 const PageNotFound = () => {
   return (
     <Layout>
-      <h1>Page Not Found</h1>
+      <div className="pnf">
+        <div className="pnf-title">404</div>
+        <div className="pnf-heading">Oops! Page does'nt found</div>
+        <Link to ="/" className="pnf-btn">GO BACK</Link>
+      </div>
     </Layout>
   );
 };
