7d7a6db3192da457b7bcbf38035c769678b439c7
Vikalp Nag <vikalpnag@gmail.com>
2024-04-23 20:14:56 +0530

    footer styling/adding some elements

--- client/src/Components/Layout/Footer.jsx
+++ client/src/Components/Layout/Footer.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import {Link} from "react-router-dom";
+
 
 const Footer = () => {
   return (
     <div className="bg-dark text-light p-3">
-      <h4 className="text-center fs-10">All rights reserverd &copy; Vikalp Nag</h4>
+      <h4 className="text-center fs-20">All rights reserverd &copy; Vikalp Nag</h4>
+      <p className="text-center mt-3 a">
+        <Link to="/about" className="links">About | </Link>
+        <Link to="/contact" className="links">Contact | </Link>
+        <Link to="/policy" className="links">Privacy Policy</Link>
+      </p>
     </div>
   );
 };
