c16a5c1aa486fa46c6ee7c653ff172c6b9efbc1e
Vikalp Nag <vikalpnag@gmail.com>
2024-05-09 18:29:58 +0530

    admin dashboard

--- client/src/Components/Layout/Footer.jsx
+++ client/src/Components/Layout/Footer.jsx
@@ -1,15 +1,22 @@
 import React from "react";
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className="bg-dark text-light p-3">
-      <h4 className="text-center fs-20">All rights reserverd &copy; Vikalp Nag</h4>
+      <h4 className="text-center fs-20">
+        All rights reserverd &copy; Vikalp Nag
+      </h4>
       <p className="text-center mt-3 a">
-        <Link to="/about" className="links">About | </Link>
-        <Link to="/contact" className="links">Contact | </Link>
-        <Link to="/policy" className="links">Privacy Policy</Link>
+        <Link to="/about" className="links">
+          About |{" "}
+        </Link>
+        <Link to="/contact" className="links">
+          Contact |{" "}
+        </Link>
+        <Link to="/policy" className="links">
+          Privacy Policy
+        </Link>
       </p>
     </div>
   );
--- client/src/Components/Routes/AdminRoute.jsx
+++ client/src/Components/Routes/AdminRoute.jsx
@@ -1,25 +1,26 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../../context/auth";
 import { Outlet } from "react-router-dom";
 import axios from "axios";
 import Spinner from "../Spinner";
 
-export const AdminRoute = ()=>{
-    const [ok,setOk]=useState(false);
-    const[auth,setAuth]=useAuth();
+export const AdminRoute = () => {
+  const [ok, setOk] = useState(false);
+  const [auth, setAuth] = useAuth();
 
-    useEffect(()=>{
-        const authCheck = async ()=>{
-            const res = await axios.get(`${process.env.REACT_APP_API}/api/v1/auth/admin-auth`);
-            if (res.data.ok){
-                setOk(true);
-            }else{
-                setOk(false);
-            }
-        };
-        if(auth?.token)authCheck();
+  useEffect(() => {
+    const authCheck = async () => {
+      const res = await axios.get(
+        `${process.env.REACT_APP_API}/api/v1/auth/admin-auth`
+      );
+      if (res.data.ok) {
+        setOk(true);
+      } else {
+        setOk(false);
+      }
+    };
+    if (auth?.token) authCheck();
+  }, [auth?.token]);
 
-    },[auth?.token]);
-
-    return ok ? <Outlet/> : <Spinner path=""/>
+  return ok ? <Outlet /> : <Spinner path="" />;
 };
