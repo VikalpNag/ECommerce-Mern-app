b6c20a0e608e01608105ccb6b6a94c15245dd3ad
Vikalp Nag <vikalpnag@gmail.com>
2024-05-29 16:24:20 +0530

    Category controller and cart Context

--- client/src/Pages/CategoryProduct.jsx
+++ client/src/Pages/CategoryProduct.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../Components/Layout/Layout";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const CategoryProduct = () => {
+  const params = useParams();
+  const [products, setProducts] = useState();
+  const [category, setCategory] = useState();
+
+  const getProductByCat = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/api/v1/product/product-category/${params.slug}`
+      );
+      setProducts(data?.products);
+      setCategory(data?.category);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Layout>
       <div className="container">
-        <h1>Cat name</h1>
+        <div className="text-center">{category?.name}</div>
       </div>
     </Layout>
   );
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -6,6 +6,7 @@ import {
     deleteProductController,
     getProductController,
     getSingleProductController,
+    productCategoryController,
     productCountController,
     productFilterController,
     productListController,
